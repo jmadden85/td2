a49e9467f37468d3aef29c67b765225e875fc14c
jmadden85 <jmadden85@gmail.com>
2015-03-13 15:34:11 -0400

    maybe switching to typescript

---
+++ js/unit.ts
@@ -0,0 +1,13 @@
+module Units {
+    class Unit {
+        id:number;
+        x:number;
+        y:number;
+        speed:number;
+        flying:boolean;
+        health:number;
+        radius:number;
+        color:string;
+    }
+    ;
+}
