1991985aca0259858b5be076a9310c6713923143
jmadden85 <jmadden85@gmail.com>
2015-03-13 15:34:24 -0400

    ts stuff

---
+++ js/unit.js
@@ -0,0 +1,10 @@
+var Units;
+(function (Units) {
+    var Unit = (function () {
+        function Unit() {
+        }
+        return Unit;
+    })();
+    ;
+})(Units || (Units = {}));
+//# sourceMappingURL=unit.js.map
